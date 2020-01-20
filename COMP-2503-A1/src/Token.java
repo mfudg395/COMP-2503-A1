67a903fa315522ee6d41ee29ab9ad83688db6b61
mfudg395 <mfudg395@B103-014.ad.mtroyal.ca>
2020-01-20 14:10:59 -0700

    Implemented overridden equals()

--- COMP-2503-A1/src/Token.java
+++ COMP-2503-A1/src/Token.java
@@ -1,4 +1,28 @@
+public class Token implements Comparable {
 
-public class Token {
+	private String word;
+	
+	public Token(String word) {
+		this.word = word;
+	}
+	
+	private String getWord() {
+		return word;
+	}
+	
+	@Override
+	public int compareTo(Object other) {
+		return 0;
+	}
+	
+	@Override
+	public boolean equals(Object other) {
+		if (other == this)
+			return true;
+		if (other == null)
+			return false;
+		Token checkToken = (Token) other;
+		return word.equals(checkToken.getWord());
+	}
 
 }
