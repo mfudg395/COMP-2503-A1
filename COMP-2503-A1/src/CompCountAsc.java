e1016831e8679f7b544d3f2f46d4a9fd6189e143
mfudg395 <mfudg395@B103-004.ad.mtroyal.ca>
2020-01-24 17:27:34 -0700

    Implemented ascending counter comparator

---
+++ COMP-2503-A1/src/CompCountAsc.java
@@ -0,0 +1,12 @@
+import java.util.Comparator;
+
+public class CompCountAsc implements Comparator<Token> {
+
+	@Override
+	public int compare(Token t1, Token t2) {
+		if (t1.getCounter() == t2.getCounter()) {
+			return t1.compareTo(t2);
+		}
+		return t2.getCounter() - t1.getCounter();
+	}
+}
